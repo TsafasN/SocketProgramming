0d0adc1c60adb0a2c2815a7beb1bdfc9f362e75e
ntsafas <ntsafas@gmail.com>
2023-12-12 01:44:33 +0200

    SP-1 Added comments to client.

--- client.c
+++ client.c
@@ -27,29 +27,35 @@ int main(int argc, char *argv[])
 	int sockfd, portno, n;
 	struct sockaddr_in serv_addr;
 	struct hostent *server;
-
 	char buffer[256];
+
+	//Check call arguments
 	if(argc < 3)
 	{
 		fprintf(stderr, "usage %s hostname port\r\n", argv[0]);
 		exit(1);
 	}
-	portno = atoi(argv[2]);
+
+	//Create file descriptor for client socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0)
 		error("ERROR opening socket");
-
+	
+	//Server ip address, get from arguments
 	server = gethostbyname(argv[1]);
 	if(server == NULL)
-	{	
 		fprintf(stderr, "Error, no such host");
-	}
 
+	//Port number, get from arguments
+	portno = atoi(argv[2]);
+
+	//Initialize sockaddr structure
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	bcopy((char *)server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
 	serv_addr.sin_port = htons(portno);
 
+	//Connect using the client socket file descriptor
 	if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("Connection Failed");
 
