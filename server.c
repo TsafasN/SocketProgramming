0ed2dddc5817263bb711ab1d078f849c69a35ec0
ntsafas <ntsafas@gmail.com>
2023-12-13 23:52:28 +0200

    SP-1 Added comments to server.

--- server.c
+++ server.c
@@ -18,37 +18,43 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
+	int sockfd, newsockfd, portno, n;
+	char buffer[255];
+	struct sockaddr_in serv_addr, cli_addr;
+	
+	//Check call arguments
 	if(argc < 2)
 	{
 		fprintf(stderr, "Port number not provided, Program terminated /r/n");
 		exit(1);
 	}
 
-	int sockfd, newsockfd, portno, n;
-	char buffer[255];
-
-	struct sockaddr_in serv_addr, cli_addr;
-	socklen_t clilen;
-
+	//Create file descriptor for server socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0)
 	{
 		error("Error opening socket.");
 	}
 
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+	//Port number, get from arguments
 	portno = atoi(argv[1]);
 
+	//Initialize sockaddr structure
+	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(portno);
 
+	//Bind using the server socket file descriptor
 	if( bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("Binding Failed.");
 
+	//Prepare to accept connections on server socket file descriptor
 	listen(sockfd, 5);
-	clilen = sizeof(cli_addr);
 
+	//Await a connection on server socket file descriptor
+	//When a connection arrives, open a new socket to communicate with it
+	socklen_t clilen = sizeof(cli_addr);
 	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 	if(newsockfd < 0)
 		error("Error on Accept.");
